46d2134d2a31160f5b2fc43fde5fdc0aa8e138b0
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2015-04-24 16:37:37 +0200

    Added class Piggyback 2

---
+++ src/main/java/se/kth/swim/msg/net/InfoPiggyback.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se.kth.swim.msg.net;
+
+import se.sics.p2ptoolbox.util.network.NatedAddress;
+
+/**
+ *
+ * @author fabriziodemaria
+ */
+
+public class InfoPiggyback {
+    
+    
+    private InfoType infoType;
+    private NatedAddress infoTarget;
+    
+    public InfoPiggyback (InfoType t, NatedAddress n){
+        this.infoType = t;
+        this.infoTarget = n;
+    }
+
+    /**
+     * @return the infoType
+     */
+    public InfoType getInfoType() {
+        return infoType;
+    }
+
+    /**
+     * @param infoType the infoType to set
+     */
+    public void setInfoType(InfoType infoType) {
+        this.infoType = infoType;
+    }
+
+    /**
+     * @return the infoTarget
+     */
+    public NatedAddress getInfoTarget() {
+        return infoTarget;
+    }
+
+    /**
+     * @param infoTarget the infoTarget to set
+     */
+    public void setInfoTarget(NatedAddress infoTarget) {
+        this.infoTarget = infoTarget;
+    }
+    
+    
+    
+}
---
+++ src/main/java/se/kth/swim/msg/net/InfoType.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se.kth.swim.msg.net;
+
+/**
+ *
+ * @author fabriziodemaria
+ */
+public enum InfoType {
+    
+     NEWNODE,
+      SUSPECTEDNODE,
+      DEADNODE
+    
+}
